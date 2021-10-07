d68cd5dfc6e039e342d9802f8c10874a88391a9e
Ganondorc <51711533+Ganondorc@users.noreply.github.com>
2021-10-07 17:24:44 -0500

    initial

---
+++ literal.h
@@ -0,0 +1,15 @@
+#pragma once
+class Literal : public Operand
+{
+public:
+	Literal(int value)
+	{
+		this->value = value;
+	}
+	double evaluate()
+	{
+		return value;
+	}
+private:
+	int value;
+};
---
+++ plus.h
@@ -0,0 +1,13 @@
+#pragma once
+class Plus : public SubExpression
+{
+public:
+	Plus(Expression* left, Expression* right) :
+		SubExpression(left, right)
+	{
+	}
+	double evaluate()
+	{
+		return left->evaluate() + right->evaluate();
+	}
+};
